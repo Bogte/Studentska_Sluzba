7e101f96f54fca399a17ce5f2205c0883591cd30
Bogte <filiplucic101@gmail.com>
2025-11-04 20:35:11 +0100

    Dodao upit za studente koji ne slusaju neki predmet ali su na nekom studijskom programu

--- src/main/java/org/raflab/studsluzba/repositories/SlusaPredmetRepository.java
+++ src/main/java/org/raflab/studsluzba/repositories/SlusaPredmetRepository.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.raflab.studsluzba.model.SlusaPredmet;
 import org.raflab.studsluzba.model.StudentIndeks;
+import org.raflab.studsluzba.model.StudentPodaci;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SlusaPredmetRepository extends CrudRepository<SlusaPredmet, Long> {
+public interface SlusaPredmetRepository extends JpaRepository<SlusaPredmet, Long> {
 	
 	@Query("select sp from SlusaPredmet sp where sp.studentIndeks.id = :indeksId1")
 	List<SlusaPredmet> getSlusaPredmetForIndeksAktivnaGodina(Long indeksId);
@@ -22,8 +25,20 @@ public interface SlusaPredmetRepository extends CrudRepository<SlusaPredmet, Lon
 	
 	@Query("select sp.studentIndeks from SlusaPredmet sp where sp.drziPredmet.id = :idDrziPredmet")
 	List<StudentIndeks> getStudentiSlusaPredmetZaDrziPredmet(Long idDrziPredmet);
-	
-	
+
+	//Upit za studente koji ne su na odredjenom studijskom programu ali ne slusaju odredjeni predmet
+	@Query("select si.student from StudentIndeks si "
+	+ "where si.studijskiProgram.naziv = :nazivPrograma "
+  	+ "and not exists (select sp from SlusaPredmet sp "
+    + "join sp.drziPredmet dp "
+    + "join dp.predmet p "
+    + "where sp.studentIndeks = si "
+    + "and p.naziv = :nazivPredmeta)")
+	List<StudentPodaci> getStudentiNaProgramuKojiNeSlusajuPredmet(
+			@Param("oznakaPrograma") String oznakaPrograma,
+			@Param("nazivPredmeta") String nazivPredmeta);
+
+
 	/*
 	 * TODO dodati slicne operacije koja vracaju sve studente za stud program/ godinu studija koje ne slusaju predmet
 	 */
